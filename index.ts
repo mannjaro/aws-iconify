6d003d157cafe6c2c365ce41275a73f39930d4a5
manjaro <takuson2525zt@gmail.com>
2024-09-23 20:31:35 +0900

    refactor

--- index.ts
+++ index.ts
@@ -4,9 +4,8 @@ import {
 	importDirectorySync,
 	cleanupSVG,
 	runSVGO,
-	parseColors,
-	isEmptyColor,
 	type IconSet,
+	type SVG,
 } from "@iconify/tools";
 import type { ImportDirectoryFileEntry } from "@iconify/tools/lib/import/directory.js";
 
@@ -18,98 +17,79 @@ function keyword(
 	const rename = defaultKeyword.replace("res-", "").replace("arch-", "");
 	return rename;
 }
-// Import icons
-const iconSet = importDirectorySync("files/svg", {
-	prefix: "aws",
-	includeSubDirs: true,
-	keyword: keyword,
-});
 
-const sizes = ["16", "32", "48", "64"];
+function processIcon(name: string, svg: SVG, iconSet: IconSet) {
+	const sizes = ["64", "48", "32"];
+	const sizeRegex = new RegExp(`-(${sizes.join("|")})$`);
+	const match = name.match(sizeRegex);
 
-// Validate, clean up, fix palette and optimise
-iconSet.forEachSync((name, type) => {
-	if (type !== "icon") {
+	if (!match) {
+		iconSet.fromSVG(name, svg);
 		return;
 	}
 
-	const svg = iconSet.toSVG(name);
-	if (!svg) {
-		// Invalid icon
-		iconSet.remove(name);
-		return;
-	}
+	const size = match[1];
+	const alias = name
+		.replace(`-${size}`, "")
+		.replace(/^amazon-/, "")
+		.replace(/^aws-/, "");
 
-	// Clean up and optimise icons
-	try {
-		// Clean up icon code
-		cleanupSVG(svg);
-
-		// Assume icon is monotone: replace color with currentColor, add if missing
-		// If icon is not monotone, remove this code
-		// parseColors(svg, {
-		// 	defaultColor: "currentColor",
-		// 	callback: (attr, colorStr, color) => {
-		// 		return !color || isEmptyColor(color) ? colorStr : "currentColor";
-		// 	},
-		// });
-
-		// Optimise
-		runSVGO(svg);
-	} catch (err) {
-		// Invalid icon
-		console.error(`Error parsing ${name}:`, err);
-		iconSet.remove(name);
-		return;
-	}
-
-	if (name.includes("64")) {
-		let alias = name.replace("-64", "");
-		alias = alias.startsWith("amazon-") ? alias.replace("amazon-", "") : alias;
+	if (size === "64" || !iconSet.exists(name.replace(size, "64"))) {
 		iconSet.fromSVG(name, svg);
 		iconSet.setAlias(alias, name);
-		return;
-	}
-	if (name.includes("-48")) {
-		let alias = name.replace("-48", "");
-		alias = alias.startsWith("amazon-") ? alias.replace("amazon-", "") : alias;
-		const baseName = name.replace("-48", "-xx");
-		const largerSize = baseName.replace("-xx", "-64");
-		if (!iconSet.exists(largerSize)) {
-			console.log("Set Alias for -48", alias);
-			iconSet.fromSVG(name, svg);
-			iconSet.setAlias(alias, name);
-		}
-		return;
-	}
-	if (name.includes("-32")) {
-		let alias = name.replace("-32", "");
-		alias = alias.startsWith("amazon-") ? alias.replace("amazon-", "") : alias;
-		const baseName = name.replace("-32", "-xx");
-		let largerSize = baseName.replace("-xx", "-64");
-		if (!iconSet.exists(largerSize)) {
-			console.log("Set Alias for -32", alias);
-			iconSet.fromSVG(name, svg);
-			iconSet.setAlias(alias, name);
-		}
-		largerSize = baseName.replace("-xx", "-48");
-		if (!iconSet.exists(largerSize)) {
-			console.log("Set Alias for -32", alias);
-			iconSet.fromSVG(name, svg);
-			iconSet.setAlias(alias, name);
-		}
-		return;
 	}
 
-	// Update icon
-	iconSet.fromSVG(name, svg);
-});
+	if (size === "32" && !iconSet.exists(name.replace("32", "48"))) {
+		iconSet.fromSVG(name, svg);
+		iconSet.setAlias(alias, name);
+	}
+}
 
-// Export
-// console.log(iconSet.export());
+const handler = () => {
+	// Import icons
+	const iconSet = importDirectorySync("files/svg", {
+		prefix: "aws",
+		includeSubDirs: true,
+		keyword: keyword,
+	});
 
-const result = iconSet.export();
-fs.writeFileSync(
-	path.join(__dirname, "./logos/aws/icons.json"),
-	JSON.stringify(result),
-);
+	// Validate, clean up, fix palette and optimise
+	iconSet.forEachSync((name, type) => {
+		if (type !== "icon") {
+			return;
+		}
+
+		const svg = iconSet.toSVG(name);
+		if (!svg) {
+			// Invalid icon
+			iconSet.remove(name);
+			return;
+		}
+
+		// Clean up and optimise icons
+		try {
+			// Clean up icon code
+			cleanupSVG(svg);
+
+			// Optimise
+			runSVGO(svg);
+		} catch (err) {
+			// Invalid icon
+			console.error(`Error parsing ${name}:`, err);
+			iconSet.remove(name);
+			return;
+		}
+		processIcon(name, svg, iconSet);
+	});
+
+	// Export
+	// console.log(iconSet.export());
+
+	const result = iconSet.export();
+	fs.writeFileSync(
+		path.join(__dirname, "./logos/aws/icons.json"),
+		JSON.stringify(result, undefined, 2),
+	);
+};
+
+handler();
